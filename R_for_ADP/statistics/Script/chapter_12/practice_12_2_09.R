b1d3a1cb92423e2441a70efa158210e34d2daa86
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-09-15 11:40:53 +0900

    두 모집단의 비교

--- R_for_ADP/statistics/Script/chapter_12/practice_12_2_09.R
+++ R_for_ADP/statistics/Script/chapter_12/practice_12_2_09.R
@@ -10,9 +10,22 @@
 data = read.csv("./statistics/Data/practice_12_2_09.csv")
 
 # (1) 평균 혈압강하량에 대한 95% 신뢰구간을 구하라.
-
-
-
+# 자료 정리
+d_bar = mean(data$d)
+n = length(data$d)
+sd = sqrt(sum((data$d-d_bar)^2)/(n-1))
+lowerbound = d_bar - qt(0.975, n-1)*sd/(sqrt(n))
+upperbound = d_bar + qt(0.975, n-1)*sd/(sqrt(n))
+# 정답 출력
+cat(paste("(", lowerbound, ",", upperbound, ")"))
 
 # (2) 이 자료로부터 약이 혈압을 내린다는 주장을 할 수 있겠는가? 
 #     유의수준 1%로 검정하라.
+# 가설
+# H_0: \delta = 0 vs H_1: \delta > 0
+# 검정통계량
+t = d_bar/(sd/sqrt(n))
+# 임곗값
+critical_value = qt(0.99, n-1)
+# 정답 출력
+cat(critical_value < t) # TRUE. 귀무가설을 기각한다.
