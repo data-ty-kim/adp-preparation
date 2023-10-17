08c7a31499994d06fee7b54c5f64536bd6f12a9f
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-10-17 16:29:28 +0900

    동질성 검정

--- R_for_ADP/statistics/Script/chapter_15/practice_15_3_05.R
+++ R_for_ADP/statistics/Script/chapter_15/practice_15_3_05.R
@@ -15,4 +15,33 @@ n_1 = rowSums(data)[1]
 n_2 = rowSums(data)[2]
 
 # 기대도수 구하기
-p_1 = n_1 * (data['A','good'] / sum(data))
+# (칸의 추정기대도수) = (칸이 속한 행의 합계)X(칸이 속한 열의 합계)/(전체합계)
+p_A1 = n_1 * (sum(data['good']) / sum(data))
+p_A2 = n_1 * (sum(data['neutral']) / sum(data))
+p_A3 = n_1 * (sum(data['bad']) / sum(data))
+p_B1 = n_2 * (sum(data['good']) / sum(data))
+p_B2 = n_2 * (sum(data['neutral']) / sum(data))
+p_B3 = n_2 * (sum(data['bad']) / sum(data))
+
+# 기대도수로 dataframe 만들기
+df_e = data.frame(good=c(p_A1, p_B1),
+                  neutral=c(p_A2, p_B2),
+                  bad=c(p_A3, p_B3),
+                  row.names=c('A', 'B')
+)
+
+# 표준화된 차이 계산하기 (O-E)^2/E
+df = (data - df_e)^2/df_e
+
+# 카이스퀘어 통계량 계산 
+chi = sum(df)
+# 자유도 계산: (행의 수 - 1) X (열의 수 - 1)
+degree_of_freedom = (nrow(df) - 1) * (ncol(df) - 1) 
+
+# 임곗값
+critical_value = qchisq(p=0.95, df=degree_of_freedom)
+
+# 정답출력
+print(critical_value < chi)  # True. H_0 기각. 두 식이요법 간의 차이는 유의하다.
+
+
